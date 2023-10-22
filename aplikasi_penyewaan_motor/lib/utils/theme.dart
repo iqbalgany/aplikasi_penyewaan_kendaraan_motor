2b062107aeab484232349a6be2cfb0209e1ad469
iqbalgany <rismagany1203@gmail.com>
2023-10-22 18:34:27 +0700

    login screen

---
+++ aplikasi_penyewaan_motor/lib/utils/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 24;
+double defaultRadius = 17;
+
+Color kPurpleColor = Color(0xff5c40cc);
+Color kBlackColor = Color(0xff1f1449);
+Color kWhiteColor = Color(0xffffffff);
+Color kGreyColor = Color(0xff9698a9);
+Color kGreenColor = Color(0xff0ec3ae);
+Color kBlueColor = Color.fromARGB(255, 43, 92, 197);
+Color kPinkColor = Color(0xffeb70a5);
+Color kRedColor = Color.fromARGB(255, 180, 19, 19);
+Color kBackgroundColor = Color(0xfffafafa);
+Color kInactiveColor = Color(0xffdbd7ec);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPurpleColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
---
+++ aplikasi_penyewaan_motor/lib/views/screens/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:aplikasi_penyewaan_motor/utils/theme.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushReplacementNamed(context, '/login-screen');
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBlueColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              margin: EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/motorcycle_icon.png'),
+                ),
+              ),
+            ),
+            Text(
+              'MotorMate',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 12,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
