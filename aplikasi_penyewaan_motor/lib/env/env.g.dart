952ae53e450c79795a813fc9aeb05cd939862a1c
iqbalgany <rismagany1203@gmail.com>
2023-11-02 19:49:29 +0700

    push

---
+++ aplikasi_penyewaan_motor/lib/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const String apiKey =
+      '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';
+}
